63ff16e7e8bf8ba3555e4696cde74bb14467195c
User Dex0ne <dm346@mail.ru>
2024-02-23 12:18:55 +0300

    //

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <div class="sm:flex sm:items-center sm:justify-between">
         <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
           <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+          <span class="self-center text-2xl font-semibold whitespace-nowrap">mweather</span>
         </a>
         <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
           <li>
